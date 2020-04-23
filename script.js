36957efe46652d5a6f7b53dc2cff3322a628ad07
iShatik <ilshat.khamatdinov@gmail.com>
2020-04-23 23:10:45 +0500

    Добавил из файла преподователя повтор задания вопроса (сброс счетчика, чтобы занов повторить вопрос)

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ for (let i = 0; i < 2; i++) {
         console.log("done");
         appData.expenses[itemExpenses] = costItemExpenses;
     } else {
+        console.log ("bad result");
     }
 }
 
