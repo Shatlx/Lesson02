f0759d4a6db2f8fb6f8c7040528b002a928f029c
iShatik <ilshat.khamatdinov@gmail.com>
2020-04-23 16:42:41 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/* let appData = {
+    money: undefined,
+    timeData: undefined,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+let money = +prompt("Ваш бюджет на месяц?", "50000"); //бюджет на месяц
+let time = prompt("Введите дату в формате YYYY-MM-DD");
+
+appData.money = money;
+appData.timeData = time;
+
+let OneItemExpenses = prompt("Введите обязательную статью расходов в этом месяце");
+let MoneyOneItemExpenses = prompt("Во сколько обойдется?");
+
+appData.expenses[OneItemExpenses] = MoneyOneItemExpenses;
+
+let TwoItemExpenses = prompt("Введите обязательную статью расходов в этом месяце");
+let MoneyTwoItemExpenses = prompt("Во сколько обойдется?");
+
+appData.expenses[TwoItemExpenses] = MoneyTwoItemExpenses;
+
+alert("Ваш бюджет на 1 день " + appData.money/30);
+
+// console.log(appData);
+
+console.log(typeof(null));
+ */
+let nnnn = prompt("Ваш бюджет на месяц?", "50000");
+console.log(typeof(nnnn));
